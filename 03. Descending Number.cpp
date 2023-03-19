f49c606b5d0b1dd1976ab9981cd9f604866061c1
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-19 20:53:50 +0600

    Create 03. Descending Number.cpp

---
+++ 03. Descending Number.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for(int i=1000; i>=1; i--)
+    {
+        cout<<i;
+        if(i%5==1)
+            cout<<"\n";
+        else
+            cout<<" ";
+    }
+}
