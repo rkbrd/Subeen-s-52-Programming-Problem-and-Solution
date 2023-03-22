3ca959e5cffaef2093f89b36548baf18c74412cb
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-22 23:24:45 +0600

    Update 07. Count Numbers.cpp

--- 07. Count Numbers.cpp
+++ 07. Count Numbers.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    cin.ignore();
+    for(int cs=1; cs<=tc; cs++)
+    {
+        string s;
+        getline(cin,s);
+        int ans=0;
+        for(int i=0; i<s.size(); i++)
+            if(s[i]==' '&&(!i||s[i-1]!=' '))  /// !i for i==0
+                ans++;
+        cout<<ans+1<<'\n';
+    }
+}
