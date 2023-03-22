6b46872619f899aa3b9473424b1cdfd913a53cff
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-22 23:10:17 +0600

    Update 04. Divisor.c

--- 04. Divisor.c
+++ 04. Divisor.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        scanf("%d",&n);
+        printf("Case %d: ",cs);
+        for(int i=1; i<=n; i++)
+            if(n%i==0)
+                printf("%d ",i);
+        printf("\n");
+    }
+}
