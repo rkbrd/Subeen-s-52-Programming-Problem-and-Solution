55692cd9408de485d5f1feea2c3ea712562990de
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-22 23:12:04 +0600

    Update 05. Box 1.c

--- 05. Box 1.c
+++ 05. Box 1.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        scanf("%d",&n);
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+        if(cs<tc)
+            printf("\n");
+    }
+}
