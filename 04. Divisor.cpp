feaaa8efdaf55859c1c6aab843eab9ca3fa543da
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-22 23:07:40 +0600

    Update 04. Divisor.cpp

--- 04. Divisor.cpp
+++ 04. Divisor.cpp
@@ -1,1 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        cin>>n;
+        cout<<"Case "<<cs<<": ";
+        for(int i=1; i<=n; i++)
+            if(n%i==0)
+                cout<<i<<' ';
+        cout<<'\n';
+    }
+}
