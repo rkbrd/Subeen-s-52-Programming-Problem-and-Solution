a0cf8f121af30d0caebb14c4ada6b313ae687d6c
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-22 23:20:48 +0600

    Update 06. Summation.c

--- 06. Summation.c
+++ 06. Summation.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        scanf("%d",&n);
+        int sum=(n%10)+(n/10000);
+        printf("Sum = %d\n",sum);
+    }
+}
