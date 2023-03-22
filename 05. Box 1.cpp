d150789aac2b572aa8e8a22692ed83c87b90d606
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-22 23:13:56 +0600

    Update 05. Box 1.cpp

--- 05. Box 1.cpp
+++ 05. Box 1.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        cin>>n;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=n; j++)
+            {
+                cout<<'*';
+            }
+            cout<<'\n';
+        }
+        if(cs<tc)
+            cout<<'\n';
+    }
+}
