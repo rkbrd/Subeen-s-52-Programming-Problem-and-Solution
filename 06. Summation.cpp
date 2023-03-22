01ce743b3f0b3bb270d300f308a3128be8738b62
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-22 23:19:46 +0600

    Update 06. Summation.cpp

--- 06. Summation.cpp
+++ 06. Summation.cpp
@@ -1,1 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        cin>>n;
+        int sum=(n%10)+(n/10000);
+        cout<<"Sum = "<<sum<<'\n';
+    }
+}
