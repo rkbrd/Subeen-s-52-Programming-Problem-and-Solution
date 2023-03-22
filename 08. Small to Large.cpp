8223e3f045e7fc16095dc821a06593d68a1b8826
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-22 23:33:22 +0600

    Update 08. Small to Large.cpp

--- 08. Small to Large.cpp
+++ 08. Small to Large.cpp
@@ -1,1 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        int mx=max(a,max(b,c));
+        int mn=min(a,min(b,c));
+        int mid=a+b+c-mx-mn;
+        cout<<"Case "<<cs<<": "<<mn<<' '<<mid<<' '<<mx<<'\n';
+    }
+}
