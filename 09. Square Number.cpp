3c3caac27bfbd8640f7fadba4ce39927ff344a83
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-22 23:43:52 +0600

    Update 09. Square Number.cpp

--- 09. Square Number.cpp
+++ 09. Square Number.cpp
@@ -1,1 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        unsigned int n;
+        cin>>n;
+        unsigned x=sqrt(n);
+        if(x*x==n)
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+}
