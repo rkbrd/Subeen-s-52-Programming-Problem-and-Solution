7f05cdf7fa36f53578de095f2859d9632b0724be
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-22 23:36:48 +0600

    Update 08. Small to Large.c

--- 08. Small to Large.c
+++ 08. Small to Large.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
 
+int max(int x,int y)
+{
+    if(x>y)
+        return x;
+    else
+        return y;
+}
+int min(int x,int y)
+{
+    if(x<y)
+        return x;
+    else
+        return y;
+}
+
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int a,b,c;
+        scanf("%d%d%d",&a,&b,&c);
+        int mx=max(a,max(b,c));
+        int mn=min(a,min(b,c));
+        int mid=a+b+c-mx-mn;
+        printf("Case %d: %d %d %d\n",cs,mn,mid,mx);
+    }
+}
