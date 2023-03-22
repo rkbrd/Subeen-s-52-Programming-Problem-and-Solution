52a038682598dbc0e7ee73e713e4b75fe1a3feb3
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-22 23:40:15 +0600

    Update 09. Square Number.c

--- 09. Square Number.c
+++ 09. Square Number.c
@@ -1,1 +1,19 @@
 
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        unsigned int n;
+        scanf("%u",&n);
+        unsigned x=sqrt(n);
+        if(x*x==n)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+}
