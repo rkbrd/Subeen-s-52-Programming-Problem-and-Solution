58795d0fa0508958c02b8596627e9e5b6f2c6404
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-22 23:30:05 +0600

    Update 07. Count Numbers.c

--- 07. Count Numbers.c
+++ 07. Count Numbers.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    getchar();
+    for(int cs=1; cs<=tc; cs++)
+    {
+        char s[100001];
+        scanf("%[^\n]",s);
+        getchar();
+        int ans=0;
+        for(int i=0; i<strlen(s); i++)
+            if(s[i]==' '&&(!i||s[i-1]!=' ')) /// !i for i==0
+                ans++;
+        printf("%d\n",ans+1);
+    }
+}
