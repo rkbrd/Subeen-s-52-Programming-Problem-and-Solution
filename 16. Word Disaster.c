33a3b482372720f62829bd8dec56ef7c731e2ac2
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-23 23:53:16 +0600

    Update 16. Word Disaster.c

--- 16. Word Disaster.c
+++ 16. Word Disaster.c
@@ -1,1 +1,36 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    getchar();
+    for(int cs=1; cs<=tc; cs++)
+    {
+        char s[1001];
+        scanf("%[^\n]s",s);
+        getchar();
+        int len=strlen(s);
+        char word[1001];
+        int in=0;
+        for(int i=0; i<len; i++)
+        {
+            if(s[i]!=' ')
+            {
+                word[in]=s[i];
+                in++;
+            }
+            else
+            {
+                for(int j=in-1; j>=0; j--)
+                    printf("%c",word[j]);
+                printf(" ");
+                in=0;
+            }
+        }
+        for(int j=in-1; j>=0; j--)
+            printf("%c",word[j]);
+        printf("\n");
+    }
+}
