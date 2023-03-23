05cc3870cc9cdf147798a43a4591d251c85823cb
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-23 18:01:01 +0600

    Create 12. Factorial 100.cpp

---
+++ 12. Factorial 100.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        cin>>n;
+        int zero=n/5+n/25;
+        cout<<zero<<endl;
+    }
+}
