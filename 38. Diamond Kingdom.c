7f1fbdcb80ecebb1a3252bb36f500d58d5f94b16
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-23 23:55:35 +0600

    Update 38. Diamond Kingdom.c

--- 38. Diamond Kingdom.c
+++ 38. Diamond Kingdom.c
@@ -1,1 +1,34 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n,m;
+        scanf("%d%d",&n,&m);
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                printf("%d",m);
+                if(j<i)
+                    printf(" ");
+            }
+            printf("\n");
+        }
+        for(int i=n-1; i>=1; i--)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                printf("%d",m);
+                if(j<i)
+                    printf(" ");
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
