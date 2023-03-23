bfeec76d27e5e49435d5f41973f2ee928e56385b
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-23 23:45:40 +0600

    Update 16. Word Disaster.cpp

--- 16. Word Disaster.cpp
+++ 16. Word Disaster.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    cin.ignore();
+    for(int cs=1; cs<=tc; cs++)
+    {
+        string s;
+        getline(cin,s);
+        stringstream ss;
+        ss<<s;
+        while(ss>>s)
+        {
+            reverse(s.begin(),s.end());
+            cout<<s<<' ';
+        }
+        cout<<'\n';
+    }
+}
