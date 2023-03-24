1c2f4fa0f861481ac7881d1cc66ef3b1d20208c0
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:19:22 +0600

    Update 17. Vowel Count.cpp

--- 17. Vowel Count.cpp
+++ 17. Vowel Count.cpp
@@ -1,1 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int is_vowel(char ch)
+{
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    int tc;
+    cin>>tc;
+    cin.ignore();
+    for(int cs=1; cs<=tc; cs++)
+    {
+        string s;
+        getline(cin,s);
+        int c=0;
+        for(int i=0; i<s.size(); i++)
+        {
+            if(is_vowel(s[i]))
+                c++;
+        }
+        cout<<"Number of vowels = "<<c<<endl;
+    }
+}
