c0c56a5fe9b94e01b923bcdbf65180eb878bb8ed
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:53:18 +0600

    Update 24. Alternate Elements.c

--- 24. Alternate Elements.c
+++ 24. Alternate Elements.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        scanf("%d",&n);
+        int ara[n];
+        for(int i=0; i<n; i++)
+            scanf("%d",&ara[i]);
+        for(int i=0; i<n; i=i+2)
+            printf("%d ",ara[i]);
+        printf("\n");
+    }
+}
