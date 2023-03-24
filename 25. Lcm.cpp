a7390762419860f884a0d1c3b794083aaf88eacd
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:55:37 +0600

    Update 25. Lcm.cpp

--- 25. Lcm.cpp
+++ 25. Lcm.cpp
@@ -1,1 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        long long a,b;
+        cin>>a>>b;
+        
+        long long ans=(a*b)/__gcd(a,b);
+        cout<<"LCM = "<<ans<<endl;
+    }
+}
