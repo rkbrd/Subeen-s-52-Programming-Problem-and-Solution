e794c4a4bc5a7a5692e080b1a26aca2e1269df6b
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:15:01 +0600

    Update 15. Character Count.cpp

--- 15. Character Count.cpp
+++ 15. Character Count.cpp
@@ -1,1 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        string s;
+        cin>>s;
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]=='#')
+                continue;
+            int c=1;
+            for(int j=i+1; j<s.size(); j++)
+                if(s[j]==s[i])
+                {
+                    c++;
+                    s[j]='#';
+                }
+            cout<<s[i]<<" = "<<c<<endl;
+        }
+        if(cs<tc)
+            cout<<endl;
+    }
+}
