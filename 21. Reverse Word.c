d54a74d31028ef83f36bc803aa8793af83cb880d
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:45:29 +0600

    Update 21. Reverse Word.c

--- 21. Reverse Word.c
+++ 21. Reverse Word.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        char s[1001];
+        scanf("%s",s);
+        for(int i=strlen(s)-1; i>=0; i--)
+            printf("%c",s[i]);
+        printf("\n");
+    }
+}
