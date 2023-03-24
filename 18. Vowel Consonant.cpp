84f12eae9096f87db71b3292323d564a021855a4
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:25:41 +0600

    Update 18. Vowel Consonant.cpp

--- 18. Vowel Consonant.cpp
+++ 18. Vowel Consonant.cpp
@@ -1,1 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int is_vowel(char ch)
+{
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    int tc;
+    cin>>tc;
+    cin.ignore();
+    for(int cs=1; cs<=tc; cs++)
+    {
+        string s;
+        getline(cin,s);
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]==' ')
+                continue;
+            if(is_vowel(s[i]))
+                cout<<s[i];
+        }
+        cout<<endl;
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]==' ')
+                continue;
+            if(!is_vowel(s[i]))
+                cout<<s[i];
+        }
+        cout<<endl;
+    }
+}
