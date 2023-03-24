21dfd8d8e09eb0d7a03a513ef0dc4bc3e4250af3
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:58:43 +0600

    Update 25. Lcm.c

--- 25. Lcm.c
+++ 25. Lcm.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
 
+int gcd(int a,int b)
+{
+    if(b==0)
+        return a;
+    if(b>a)
+    {
+        int temp=a;
+        a=b;
+        b=temp;
+    }
+    return gcd(b,a%b);
+}
+
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        long long a,b;
+        scanf("%lld %lld",&a,&b);
+        
+        long long ans=(a*b)/gcd(a,b);
+        printf("LCM = %lld\n",ans);
+    }
+}
