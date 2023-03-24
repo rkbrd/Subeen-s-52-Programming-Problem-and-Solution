570d5a0cd865b7be9d12be17fd025991a5f2e461
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 02:15:56 +0600

    Update 49. Is Prime.cpp

--- 49. Is Prime.cpp
+++ 49. Is Prime.cpp
@@ -1,1 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+bool is_prime(long long n)
+{
+    if(n==1)
+        return false;
+    if(n==2)
+        return true;
+    for(long long i=2; i*i<=n; i++)
+        if(n%i==0)
+            return false;
+    return true;
+}
+
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        long long n;
+        cin>>n;
+        if(is_prime(n))
+            cout<<n<<" is a prime\n";
+        else
+            cout<<n<<" is not a prime\n";
+    }
+}
