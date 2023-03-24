629f99dd23a3bf1b96b3702d78779b7e37ba3c39
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:27:24 +0600

    Update 19. Word Count 1.cpp

--- 19. Word Count 1.cpp
+++ 19. Word Count 1.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+
+int main()
+{
+    int tc;
+    cin>>tc;
+    cin.ignore();
+    for(int cs=1; cs<=tc; cs++)
+    {
+        string s;
+        getline(cin,s);
+        int c=0;
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]==' ')
+                c++;
+        }
+        cout<<"Count = "<<c+1<<endl;
+    }
+}
