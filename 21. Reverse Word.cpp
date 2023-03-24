90685e38b2c18f65510667f4695452edcc282316
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:44:13 +0600

    Update 21. Reverse Word.cpp

--- 21. Reverse Word.cpp
+++ 21. Reverse Word.cpp
@@ -1,1 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        string s;
+        cin>>s;
+        reverse(s.begin(),s.end());
+        cout<<s<<endl;
+    }
+}
