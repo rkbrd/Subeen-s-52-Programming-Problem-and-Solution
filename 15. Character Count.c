61dbcc7eec14dfc95b901f86f1efb67a24ffed33
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:13:30 +0600

    Update 15. Character Count.c

--- 15. Character Count.c
+++ 15. Character Count.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+#include<string.h>
 
+
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        char s[1001];
+        scanf("%s",s);
+        for(int i=0; i<strlen(s); i++)
+        {
+            if(s[i]=='#')
+                continue;
+            int c=1;
+            for(int j=i+1; j<strlen(s); j++)
+                if(s[j]==s[i])
+                {
+                    c++;
+                    s[j]='#';
+                }
+            printf("%c = %d\n",s[i],c);
+        }
+        if(cs<tc)
+            printf("\n");
+    }
+}
