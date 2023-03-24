6871fa6b9c146b59f7a9e58a7cf6bf6cb2bb0bc0
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:39:24 +0600

    Update 14. Character Frequency.c

--- 14. Character Frequency.c
+++ 14. Character Frequency.c
@@ -1,1 +1,27 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    getchar();
+    for(int cs=1; cs<=tc; cs++)
+    {
+        char s[10001],c[2];
+        scanf("%[^\n]s",s);
+        getchar();
+        scanf("%[^\n]s",c);
+        getchar();
+        int count=0;
+        for(int i=0; i<strlen(s); i++)
+        {
+            if(s[i]==c[0])
+                count++;
+        }
+        if(count>0)
+            printf("Occurrence of '%c' in '%s' = %d\n",c[0],s,count);
+        else
+            printf("'%c' is not present\n",c[0]);
+    }
+}
