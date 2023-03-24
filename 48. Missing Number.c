0da3dba3e149262aa26a87824b4aa87c688729b9
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 02:22:10 +0600

    Update 48. Missing Number.c

--- 48. Missing Number.c
+++ 48. Missing Number.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        long long n;
+        scanf("%lld",&n);
+        long long sum=(n*(n+1))/2;
+        for(int i=1; i<n; i++)
+        {
+            int a;
+            scanf("%d",&a);
+            sum=sum-a;
+        }
+        printf("%lld\n",sum);
+    }
+}
