583adfbb2a2e928a89f63e85db4bd62326b69585
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:51:42 +0600

    Update 24. Alternate Elements.cpp

--- 24. Alternate Elements.cpp
+++ 24. Alternate Elements.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        cin>>n;
+        vector<int>ara(n);
+        for(int i=0; i<n; i++)
+            cin>>ara[i];
+        for(int i=0; i<n; i=i+2)
+            cout<<ara[i]<<' ';
+        cout<<endl;
+    }
+}
