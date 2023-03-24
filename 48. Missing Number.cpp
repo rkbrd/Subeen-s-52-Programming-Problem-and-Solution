8382dd08de0279ac9c6692fc8a9dcb3ffa9d4a51
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 02:22:54 +0600

    Update 48. Missing Number.cpp

--- 48. Missing Number.cpp
+++ 48. Missing Number.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        long long n;
+        cin>>n;
+        long long sum=(n*(n+1))/2;
+        for(int i=0; i<n; i++)
+        {
+            int a;
+            scanf("%d",&a);
+            sum=sum-a;
+        }
+        cout<<sum<<endl;
+    }
+}
