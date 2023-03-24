510ec8accbecf4bf61436eb36b26a60b0714773e
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:47:43 +0600

    Update 23. Letter to Number.c

--- 23. Letter to Number.c
+++ 23. Letter to Number.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        char s[101];
+        scanf("%s",s);
+        for(int i=0;i<strlen(s);i++)
+            printf("%d",s[i]-'A'+1);
+        printf("\n");
+    }
+}
