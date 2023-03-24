a699d2252cc12798f0b5ec8091cb8e77d60da349
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 02:26:22 +0600

    Update 50. Left Right.cpp

--- 50. Left Right.cpp
+++ 50. Left Right.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        string s;
+        cin>>s;
+        int n=s.size();
+        for(int i=1; i<n; i++)
+            if(s[i]=='L')
+                s[i]=s[i-1];
+        for(int i=n-2; i>=0; i--)
+            if(s[i]=='R')
+                s[i]=s[i+1];
+        cout<<s<<endl;
+    }
+}
