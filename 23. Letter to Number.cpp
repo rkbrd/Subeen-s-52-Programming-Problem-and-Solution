20d4e37258fff903f1d8b76bea614527fd110a19
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:48:51 +0600

    Update 23. Letter to Number.cpp

--- 23. Letter to Number.cpp
+++ 23. Letter to Number.cpp
@@ -1,1 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        string s;
+        cin>>s;
+        for(int i=0;i<s.size();i++)
+            cout<<(int)(s[i]-'A'+1);
+        cout<<endl;
+    }
+}
