f1843b0ff9f5d8a2e8b59857a1dba19cf8ef71ce
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 01:29:40 +0600

    Update 20. Word Count 2.c

--- 20. Word Count 2.c
+++ 20. Word Count 2.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    getchar();
+    for(int cs=1; cs<=tc; cs++)
+    {
+        char s[10001];
+        scanf("%[^\n]s",s);
+        getchar();
+        int c=0;
+        for(int i=0; i<strlen(s); i++)
+        {
+            if(s[i]==' ')
+                c++;
+        }
+        printf("Count = %d\n",c+1);
+    }
+}
