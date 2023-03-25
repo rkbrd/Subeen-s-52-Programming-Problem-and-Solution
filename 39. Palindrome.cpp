9aca7a6aef91027ce4ab97a921ec37c456aaab29
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-26 00:24:48 +0600

    Update 39. Palindrome.cpp

--- 39. Palindrome.cpp
+++ 39. Palindrome.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        string s;
+        cin>>s;
+        string t=s;
+        reverse(t.begin(),t.end());
+        if(s==t)
+            cout<<"Yes! It is palindrome!"<<endl;
+        else
+            cout<<"Sorry! It is not palindrome!"<<endl;
+    }
+}
