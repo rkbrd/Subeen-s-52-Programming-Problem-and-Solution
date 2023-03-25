f5451ecdf129cd854e1c58b2105c5cf5693fde86
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 23:32:47 +0600

    Update 27. Armstrong Number.c

--- 27. Armstrong Number.c
+++ 27. Armstrong Number.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n,x,sum=0;
+        scanf("%d",&n);
+        x=n;
+        while(n>0)
+        {
+            int rem=n%10;
+            sum=sum+(rem*rem*rem);
+            n=n/10;
+        }
+        if(sum==x)
+            printf("%d is an armstrong number!\n",x);
+        else
+            printf("%d is not an armstrong number!\n",x);
+    }
+}
