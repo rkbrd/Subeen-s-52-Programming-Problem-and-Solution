71620c60356c16ad00f6ed0f6e98d17a0e58e299
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 23:38:28 +0600

    Update 28. Random Array.cpp

--- 28. Random Array.cpp
+++ 28. Random Array.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        cin>>n;
+        vector<int>ara(n);
+        for(int i=0; i<n; i++)
+            cin>>ara[i];
+        int inc=1,dec=1;
+        for(int i=1; i<n; i++)
+            if(ara[i]<ara[i-1])
+            {
+                inc=0;
+                break;
+            }
+        for(int i=n-2; i>=0; i--)
+            if(ara[i]<ara[i+1])
+            {
+                dec=0;
+                break;
+            }
+        if(inc==1||dec==1)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+}
