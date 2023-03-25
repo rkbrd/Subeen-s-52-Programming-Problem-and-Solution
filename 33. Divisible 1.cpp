fa894bef435676ee8ad3f62cf795318de9169a53
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-26 00:13:20 +0600

    Update 33. Divisible 1.cpp

--- 33. Divisible 1.cpp
+++ 33. Divisible 1.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        long long a,b,c;
+        cin>>a>>b>>c;
+        long long i=a/c;
+        if(c*i<a)
+            i++;
+        while(i*c<=b)
+        {
+            cout<<i*c<<endl;
+            i++;
+        }
+        cout<<endl;
+    }
+}
