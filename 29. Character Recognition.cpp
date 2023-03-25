160f0f8035f738b06fd80fdb5150993c5be42205
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 23:42:36 +0600

    Update 29. Character Recognition.cpp

--- 29. Character Recognition.cpp
+++ 29. Character Recognition.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        char ch;
+        cin>>ch;
+        if(isupper(ch))
+            cout<<"Uppercase Character"<<endl;
+        else if(islower(ch))
+            cout<<"Lowercase Character"<<endl;
+        else if(isdigit(ch))
+            cout<<"Numerical Digit"<<endl;
+        else
+            cout<<"Special Characters"<<endl;
+    }
+}
