1dec6b3a377df48793ddc5078c996af31bf6a13d
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-26 00:31:18 +0600

    Update 39. Palindrome.c

--- 39. Palindrome.c
+++ 39. Palindrome.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        char s[1001];
+        scanf("%s",s);
+        int ok=1;
+        int i=0,j=strlen(s)-1;
+        while(i<=j)
+        {
+            if(s[i]!=s[j])
+            {
+                ok=0;
+                break;
+            }
+            i++;
+            j--;
+        }
+        if(ok==1)
+            printf("Yes! It is palindrome!\n");
+        else
+            printf("Sorry! It is not palindrome!\n");
+    }
+}
