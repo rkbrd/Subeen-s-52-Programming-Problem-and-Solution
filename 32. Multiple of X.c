ae2014a79865cfbba65a405f6ae764134d80d336
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-26 00:00:11 +0600

    Update 32. Multiple of X.c

--- 32. Multiple of X.c
+++ 32. Multiple of X.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int x,n;
+        scanf("%d%d",&x,&n);
+        if(x>n)
+            printf("Invalid!\n");
+        else
+        {
+            for(int i=1; i*x<=n; i++)
+                printf("%d\n",x*i);
+        }
+        printf("\n");
+    }
+}
