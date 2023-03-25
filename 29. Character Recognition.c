02df35b09c7bdf678668ed2577373cdad3ff6f3e
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 23:48:08 +0600

    Update 29. Character Recognition.c

--- 29. Character Recognition.c
+++ 29. Character Recognition.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    getchar();
+    for(int cs=1; cs<=tc; cs++)
+    {
+        char ch;
+        scanf("%c",&ch);
+        getchar();
+        if(ch>='A'&&ch<='Z')
+            printf("Uppercase Character\n");
+        else if(ch>='a'&&ch<='z')
+            printf("Lowercase Character\n");
+        else if(ch>='0'&&ch<='9')
+            printf("Numerical Digit\n");
+        else
+            printf("Special Characters\n");
+    }
+}
