e7e00f7071d86eaf4f337d026bcfb9a967436888
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 23:22:43 +0600

    Update 26. Alien Gupi.cpp

--- 26. Alien Gupi.cpp
+++ 26. Alien Gupi.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        double x;
+        cin>>x;
+        int day=0;
+        while(x>1.0)
+        {
+            day++;
+            x=x/2.0;
+        }
+        cout<<day<<" days"<<endl;
+    }
+}
