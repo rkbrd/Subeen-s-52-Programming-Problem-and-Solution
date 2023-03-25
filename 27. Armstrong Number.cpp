e7661fbb89adbd091e707a3c47b778aa4734311c
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 23:30:02 +0600

    Update 27. Armstrong Number.cpp

--- 27. Armstrong Number.cpp
+++ 27. Armstrong Number.cpp
@@ -1,1 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n,x,sum=0;
+        cin>>n;
+        x=n;
+        while(n>0)
+        {
+            int rem=n%10;
+            sum=sum+(rem*rem*rem);
+            n=n/10;
+        }
+        if(sum==x)
+            cout<<x<<" is an armstrong number!"<<endl;
+        else
+            cout<<x<<" is not an armstrong number!"<<endl;
+    }
+}
