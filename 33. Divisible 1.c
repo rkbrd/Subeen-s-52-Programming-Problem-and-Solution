dccd4e0ae149638f083f3653c3b40864e6d322fb
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-26 00:11:36 +0600

    Update 33. Divisible 1.c

--- 33. Divisible 1.c
+++ 33. Divisible 1.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        long long a,b,c;
+        scanf("%lld%lld%lld",&a,&b,&c);
+        long long i=a/c;
+        if(c*i<a)
+            i++;
+        while(i*c<=b)
+        {
+            printf("%lld\n",i*c);
+            i++;
+        }
+        printf("\n");
+    }
+}
