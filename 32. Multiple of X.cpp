b9425e53a8cda73906f0506d4daff4a59a17e14d
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 23:55:30 +0600

    Update 32. Multiple of X.cpp

--- 32. Multiple of X.cpp
+++ 32. Multiple of X.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int x,n;
+        cin>>x>>n;
+        if(x>n)
+            cout<<"Invalid!"<<endl;
+        else
+        {
+            for(int i=1; i*x<=n; i++)
+                cout<<x*i<<endl;
+        }
+        cout<<endl;
+    }
+}
