f4948e7be6a417ad731a40bb4b7c2b9c5a25c932
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-25 23:21:04 +0600

    Update 26. Alien Gupi.c

--- 26. Alien Gupi.c
+++ 26. Alien Gupi.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        double x;
+        scanf("%lf",&x);
+        int day=0;
+        while(x>1.0)
+        {
+            day++;
+            x=x/2.0;
+        }
+        printf("%d days\n",day);
+    }
+}
