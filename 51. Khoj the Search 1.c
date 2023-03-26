76e500d3e95e9fa8b3725c178a465dd126fc8e6f
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 02:09:05 +0600

    Update 51. Khoj the Search 1.c

--- 51. Khoj the Search 1.c
+++ 51. Khoj the Search 1.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        char s[128],t[128];
+        scanf("%s%s",s,t);
+        int position;
+        int n=strlen(t);
+        for(int i=0; i+n<=strlen(s); i++)
+        {
+            int same=0;
+            for(int j=0; j<n; j++)
+                if(s[i+j]==t[j])
+                    same++;
+                else
+                    break;
+            if(same==n)
+            {
+                position=i;
+                break;
+            }
+        }
+        printf("%d\n",position);
+    }
+}
