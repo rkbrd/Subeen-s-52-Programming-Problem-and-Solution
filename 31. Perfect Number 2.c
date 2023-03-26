09e1f65650c33ea7aa230e075f4bfbb967ca83cc
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 03:34:43 +0600

    Update 31. Perfect Number 2.c

--- 31. Perfect Number 2.c
+++ 31. Perfect Number 2.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        scanf("%d",&n);
+        int ara[]= {6,28,496,8128,33550336};
+        int ok=0;
+        for(int i=0; i<5; i++)
+            if(ara[i]<=n)
+                printf("%d\n",ara[i]);
+        printf("\n");
+    }
+}
