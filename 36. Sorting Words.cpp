12a10f768bf79f38b8a98b5ac31f118a2dddcc11
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 03:09:08 +0600

    Update 36. Sorting Words.cpp

--- 36. Sorting Words.cpp
+++ 36. Sorting Words.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        cin>>n;
+        vector<string> ara(n);
+        for(int i=0; i<n; i++)
+            cin>>ara[i];
+        sort(ara.begin(),ara.end());
+        for(int i=0; i<n; i++)
+            cout<<ara[i]<<endl;
+    }
+}
