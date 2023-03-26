692baa5363331e10d47528b7b82bb1a2c3d19948
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 02:19:30 +0600

    Update 47. Coalition of Array.c

--- 47. Coalition of Array.c
+++ 47. Coalition of Array.c
@@ -1,1 +1,52 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n,m;
+        scanf("%d",&n);
+        int a[n];
+        for(int i=0; i<n; i++)
+            scanf("%d",&a[i]);
+        scanf("%d",&m);
+        int b[m];
+        for(int i=0; i<m; i++)
+            scanf("%d",&b[i]);
+        int c[n+m];
+        int k=0,i=0,j=0;
+        while(i<n&&j<m)
+        {
+            if(a[i]<=b[j])
+            {
+                c[k]=a[i];
+                i++;
+                k++;
+            }
+            else
+            {
+                c[k]=b[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<n)
+        {
+            c[k]=a[i];
+            i++;
+            k++;
+        }
+        while(j<m)
+        {
+            c[k]=b[j];
+            j++;
+            k++;
+        }
+        for(int i=0; i<n+m; i++)
+            printf("%d ",c[i]);
+        printf("\n");
+    }
+}
