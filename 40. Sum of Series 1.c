bca7a6ddbf8d14d230eeb16092c7ecedcfc68d02
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 02:28:30 +0600

    Update 40. Sum of Series 1.c

--- 40. Sum of Series 1.c
+++ 40. Sum of Series 1.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+#include<math.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int x,k,ans=0;
+        scanf("%d%d",&x,&k);
+        for(int i=0; i<=k; i++)
+        {
+            int mul=1;
+            for(int j=1; j<=i; j++)
+                mul=mul*x;
+            ans=ans+mul;
+        }
+        printf("Result = %d\n",ans);
+    }
+}
