a6a277b09f34be68a5619d5653f86ec211e28544
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 02:32:37 +0600

    Update 41. Sum of Series 2.c

--- 41. Sum of Series 2.c
+++ 41. Sum of Series 2.c
@@ -1,1 +1,27 @@
+#include<stdio.h>
 
+long long factorial(long long n)
+{
+    long long fact=1;
+    for(int i=1; i<=n; i++)
+        fact=fact*i;
+    return fact;
+}
+
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        long long n;
+        scanf("%lld",&n);
+        double ans=0;
+        for(int i=1; i<=n; i++)
+        {
+            double now=1.0*i/factorial(i);
+            ans=ans+now;
+        }
+        printf("%.4lf\n",ans);
+    }
+}
