2b791fbe6752535576735f1ea83b4fae4545615c
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 02:51:51 +0600

    Update 42. Sum of Series 3.cpp

--- 42. Sum of Series 3.cpp
+++ 42. Sum of Series 3.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        cin>>n;
+        for(int i=n; i>=0; i--)
+        {
+            if(i>1)
+                cout<<"2^"<<i<<" + ";
+            else if(i==1)
+                cout<<"2 + ";
+            else
+                cout<<"1\n";
+        }
+    }
+}
