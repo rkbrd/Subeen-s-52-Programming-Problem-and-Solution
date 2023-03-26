b4623e67102b5629c0f21b0fd64e391ac09b5f4f
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 03:32:43 +0600

    Update 31. Perfect Number 2.cpp

--- 31. Perfect Number 2.cpp
+++ 31. Perfect Number 2.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        cin>>n;
+        vector<int> ara= {6,28,496,8128,33550336};
+        int ok=0;
+        for(int i=0; i<ara.size(); i++)
+            if(ara[i]<=n)
+                cout<<ara[i]<<endl;
+        cout<<endl;
+    }
+}
