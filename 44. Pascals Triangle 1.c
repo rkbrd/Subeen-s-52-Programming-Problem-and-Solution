bbc9b019c11700e12137997eb1e2b7e836742612
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 03:00:26 +0600

    Update 44. Pascals Triangle 1.c

--- 44. Pascals Triangle 1.c
+++ 44. Pascals Triangle 1.c
@@ -1,1 +1,27 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        scanf("%d",&n);
+        int ara[n+1][n+1];
+        ara[0][0]=1;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<i; j++)
+                ara[i][j]=ara[i-1][j-1]+ara[i-1][j];
+            ara[i][i]=ara[i][0]=1;
+        }
+        for(int i=0; i<=n; i++)
+        {
+            for(int j=0; j<=i; j++)
+                printf("%d ",ara[i][j]);
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
