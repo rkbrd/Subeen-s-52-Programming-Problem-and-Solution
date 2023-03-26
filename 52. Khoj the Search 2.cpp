ef307fca3ae811edca5273c7c14ee215b4379606
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 02:03:48 +0600

    Update 52. Khoj the Search 2.cpp

--- 52. Khoj the Search 2.cpp
+++ 52. Khoj the Search 2.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        string s,t;
+        cin>>s>>t;
+        int c=0;
+        int n=t.size();
+        for(int i=0; i+n<=s.size(); i++)
+            if(s.substr(i,n)==t)
+                c++;
+        cout<<c<<endl;
+    }
+}
