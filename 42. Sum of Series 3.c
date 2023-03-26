d634671609cbd8a9f5499f1b9cbc8ccdfb4af3f7
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 02:54:09 +0600

    Update 42. Sum of Series 3.c

--- 42. Sum of Series 3.c
+++ 42. Sum of Series 3.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        scanf("%d",&n);
+        for(int i=n; i>=0; i--)
+        {
+            if(i>1)
+                printf("2^%d + ",i);
+            else if(i==1)
+                printf("2 + ");
+            else
+                printf("1\n");
+        }
+    }
+}
