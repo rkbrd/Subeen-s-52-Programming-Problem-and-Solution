406c80cdab80fb020bdd1671750d183e4d55632c
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 02:46:29 +0600

    Update 35. Out of Circle.cpp

--- 35. Out of Circle.cpp
+++ 35. Out of Circle.cpp
@@ -1,1 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+double distance(int x1,int y1,int x2,int y2)
+{
+    long long totat=1LL*(x1-x2)*(x1-x2)+1LL*(y1-y2)*(y1-y2);
+    return sqrtl(totat);
+}
+
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int a,b,x,y,r;
+        cin>>x>>y>>r>>a>>b;
+        double dist=distance(x,y,a,b);
+        if(dist<=1.0*r)
+            cout<<"The point is inside the circle\n";
+        else
+            cout<<"The point is not inside the circle\n";
+    }
+}
