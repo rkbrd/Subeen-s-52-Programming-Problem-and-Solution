196e099310314bf87e09ff21b230ddbaf0be95f2
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 02:35:33 +0600

    Update 37. Number Disaster.cpp

--- 37. Number Disaster.cpp
+++ 37. Number Disaster.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        cin>>n;
+        int ans=0;
+        while(n>0)
+        {
+            int rem=n%10;
+            ans=ans*10+rem;
+            n=n/10;
+        }
+        cout<<ans<<endl;
+    }
+}
