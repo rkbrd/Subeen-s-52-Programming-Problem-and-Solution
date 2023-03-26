98283ee843d19296d4fa3f1b4e120d0ab31b43e6
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 02:22:56 +0600

    Update 46. Area of Triangle.c

--- 46. Area of Triangle.c
+++ 46. Area of Triangle.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+#include<math.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int a,b,c;
+        scanf("%d%d%d",&a,&b,&c);
+        double s=(a+b+c)/2.0;
+        double area=s*(s-a)*(s-b)*(s-c);
+        area=sqrt(area);
+        printf("Area = %.3lf\n",area);
+    }
+}
