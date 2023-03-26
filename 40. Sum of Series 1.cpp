b425c5fd3421ebc84969217056e7482f6faeba50
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 02:27:25 +0600

    Update 40. Sum of Series 1.cpp

--- 40. Sum of Series 1.cpp
+++ 40. Sum of Series 1.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int x,k,ans=0;
+        cin>>x>>k;
+        for(int i=0; i<=k; i++)
+        {
+            int mul=1;
+            for(int j=1; j<=i; j++)
+                mul=mul*x;
+            ans=ans+mul;
+        }
+        cout<<"Result = "<<ans<<endl;
+    }
+}
