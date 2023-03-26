4215e69e41f815697f4590cb91337e59b286766b
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 03:17:35 +0600

    Update 43. Calculation.c

--- 43. Calculation.c
+++ 43. Calculation.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int p,q,c;
+        scanf("%d%d%d",&p,&q,&c);
+        int ans=1;
+        for(int i=1; i<=q; i++)
+            ans=(ans*p)%c;
+        printf("Result = %d\n",ans);
+    }
+}
