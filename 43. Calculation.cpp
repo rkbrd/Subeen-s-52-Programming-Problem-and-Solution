f0103f7f31a901aec87c35ff52ba5f2c33480e81
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 03:18:32 +0600

    Update 43. Calculation.cpp

--- 43. Calculation.cpp
+++ 43. Calculation.cpp
@@ -1,1 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int p,q,c;
+        cin>>p>>q>>c;
+        int ans=1;
+        for(int i=1; i<=q; i++)
+            ans=(ans*p)%c;
+        cout<<"Result = "<<ans<<endl;
+    }
+}
