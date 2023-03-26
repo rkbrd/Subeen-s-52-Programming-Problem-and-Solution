91d634a841363069ae34191165b69915dc54291b
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 02:24:21 +0600

    Update 46. Area of Triangle.cpp

--- 46. Area of Triangle.cpp
+++ 46. Area of Triangle.cpp
@@ -1,1 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        double s=(a+b+c)/2.0;
+        double area=s*(s-a)*(s-b)*(s-c);
+        area=sqrt(area);
+        cout<<fixed<<setprecision(3);
+        cout<<"Area = "<<area<<endl;
+    }
+}
