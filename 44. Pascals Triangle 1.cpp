598459e2a9e2369b3b3c0ccb383c5e82789936ab
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 03:01:57 +0600

    Update 44. Pascals Triangle 1.cpp

--- 44. Pascals Triangle 1.cpp
+++ 44. Pascals Triangle 1.cpp
@@ -1,1 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int tc;
+    cin>>tc;
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int>> ara(n+1,vector<int>(n+1));
+        ara[0][0]=1;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<i; j++)
+                ara[i][j]=ara[i-1][j-1]+ara[i-1][j];
+            ara[i][i]=ara[i][0]=1;
+        }
+        for(int i=0; i<=n; i++)
+        {
+            for(int j=0; j<=i; j++)
+                cout<<ara[i][j]<<' ';
+            cout<<endl;
+        }
+        cout<<endl;
+    }
+}
