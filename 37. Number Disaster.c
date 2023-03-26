06d85bf2eaa747de191394b34e36d4c10b689a0d
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-27 02:36:39 +0600

    Update 37. Number Disaster.c

--- 37. Number Disaster.c
+++ 37. Number Disaster.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        int n;
+        scanf("%d",&n);
+        int ans=0;
+        while(n>0)
+        {
+            int rem=n%10;
+            ans=ans*10+rem;
+            n=n/10;
+        }
+        printf("%d\n",ans);
+    }
+}
