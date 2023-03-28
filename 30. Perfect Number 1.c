ea795dddf8ae69401274e6837074679ab84d2d3a
Rakibul Islam Riyad <55946275+rkbrd@users.noreply.github.com>
2023-03-28 15:54:09 +0600

    Update 30. Perfect Number 1.c

--- 30. Perfect Number 1.c
+++ 30. Perfect Number 1.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    for(int cs=1; cs<=tc; cs++)
+    {
+        char n[21];
+        scanf("%s",n);
+        char *ara[]= {"6","28","496","8128","33550336","8589869056","137438691328","2305843008139952128"};
+        int ok=0;
+        for(int i=0; i<sizeof(*ara); i++)
+            if(strcmp(n,ara[i])==0)
+                ok=1;
+        if(ok==1)
+            printf("YES, %s is a perfect number!\n",n);
+        else
+            printf("NO, %s is not a perfect number!\n",n);
+    }
+}
